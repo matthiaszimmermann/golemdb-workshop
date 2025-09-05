3da44b9701ffb0be3cad65211dab22d19d898854
Krzysztof Fonał <krzysiekfonal@gmail.com>
2025-09-05 11:30:58 +0200

    Change entity hash for TS

--- samples/python/hello_world.py
+++ samples/python/hello_world.py
@@ -6,7 +6,7 @@ async def hello_world():
         "https://ethwarsaw.holesky.golemdb.io/rpc", 
         "wss://ethwarsaw.holesky.golemdb.io/rpc/ws"
     )
-    print(await client.get_storage_value(GenericBytes.from_hex_string("0xf4888ec1ffb3f8bc794f6f0d61b887e2bd3d0ac9e81f8ba63807d1f3497285ee")))
+    print(await client.get_storage_value(GenericBytes.from_hex_string("0xda66a86e61dd6bb570d62a59e8b9310e4cb2e89cb1215b8b8243ac4f6cffc4c3")))
     await client.disconnect()
 
 asyncio.run(hello_world())
--- samples/ts/index.ts
+++ samples/ts/index.ts
@@ -1,13 +1,13 @@
 import { createROClient } from "golem-base-sdk";
 
 const client = createROClient(
-    60138453025,
+    60138453033,
     "https://ethwarsaw.holesky.golemdb.io/rpc",
     "wss://ethwarsaw.holesky.golemdb.io/rpc/ws"
 );
 
 const decoder = new TextDecoder();
-client.getStorageValue("0xf4888ec1ffb3f8bc794f6f0d61b887e2bd3d0ac9e81f8ba63807d1f3497285ee").then(value => {
+client.getStorageValue("0xda66a86e61dd6bb570d62a59e8b9310e4cb2e89cb1215b8b8243ac4f6cffc4c3").then(value => {
     console.log(decoder.decode(value));
 });
 
